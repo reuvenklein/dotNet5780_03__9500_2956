b28caf1d555acd17a9e20ddfee451c2ca02b2d32
Reuven Klein <kleinreuven@gmail.com>
2019-12-10 22:30:58 +0200

    Started question number 3

---
+++ dotnet5780_03_9500_2956/dotnet5780_03_9500_2956/Host.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5780_03_9500_2956
+{
+  public  class Host
+    {
+        public string HostName { get; set; }
+        public List<HostingUnit>units { get; set; }
+    }
+}
---
+++ dotnet5780_03_9500_2956/dotnet5780_03_9500_2956/HostingUnit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5780_03_9500_2956
+{
+  public  class HostingUnit
+    {
+        public string UnitName { get; set; }
+        public int Rooms { get; set; }
+        public bool IsSwimmimgPool  { get; set; }
+        public List<DateTime> AllOrders { get; set; }
+        public List<string> Uris { get; set; }
+
+
+    }
+
+}
